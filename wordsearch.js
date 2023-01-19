d8bc6f9e47ae4704d1ca679028e50e68f2788261
JimHwkins <howlarrior@gmail.com>
2023-01-18 23:01:34 -0300

    Assignment Completed

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  }
 
-module.exports = wordSearch
+  const horizontalJoin = letters.map((rowLetter) => rowLetter.join(""));
+  const verticalJoin = transpose(letters).map((columnLetter) =>
+    columnLetter.join("")
+  );
+  let found = false;
+
+  for (const string of horizontalJoin) {
+    if (string.includes(word)) found = true;
+  }
+  for (const string of verticalJoin) {
+    if (string.includes(word)) found = true;
+  }
+
+  return found;
+};
+const transpose = function (matrix) {
+  // Put your solution here
+  const newArray = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    newArray.push([]);
+  }
+  for (let x = 0; x < matrix.length; x++) {
+    for (let y = 0; y < matrix[0].length; y++) {
+      newArray[y].push(matrix[x][y]);
+    }
+  }
+  return newArray;
+
+  // return matrix[0].map((colum, c) => matrix.map((row, r) => matrix[r][c]));
+};
+
+module.exports = wordSearch;
